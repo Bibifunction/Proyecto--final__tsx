4856aa93332e880e856aab06a4b1a102ed1c8f2f
Bita Taghavi <whaybi2011@gmail.com>
2025-04-17 17:32:20 +0200

    add logo in header

--- Login.tsx/src/components/Header.tsx
+++ Login.tsx/src/components/Header.tsx
@@ -1,9 +1,10 @@
 import "./styles/Header.css"
+import logo from "../assets/logo.svg"
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="header__logo"></div>
+      <img src={logo || "/placeholder.svg"} alt="PawPlay Logo" className="header__logo" />
     </header>
   )
 }
